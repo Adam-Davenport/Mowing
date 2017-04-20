b8eca7b48f123323af1b2209b2ee76a5912bf01c
Adam-Davenport <adavenpo@purdue.edu>
2017-04-20 17:40:17 -0400

    Rendering templates at the appropriate locations

--- record/views.py
+++ record/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def farm(request):
+    return render(request, 'farm.html')
+
+def trails(request):
+    return render(request, 'farm.html')
