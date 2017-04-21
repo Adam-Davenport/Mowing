b52870ffc3e065aeea1013d2621fbd42a7e7dd6c
Adam-Davenport <adavenpo@purdue.edu>
2017-04-20 22:15:58 -0400

    Setting up index page and post handling

--- record/views.py
+++ record/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from record.models import Mowing
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        post_record(request)
+    records = Mowing.objects.order_by('date').reverse()
+    context = {
+        'records': records
+    }
+    return render(request, 'index.html', context)
+
+def post_record(request):
+    print(request.POST)
+    location = request.POST['location']
+    direction = request.POST['direction']
+    session = Mowing.objects.create(location=location, direction=direction)
+    session.save()
 
 def home(request):
     return render(request, 'home.html')
