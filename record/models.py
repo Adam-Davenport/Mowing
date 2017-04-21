eeb433efb5604d70bb7ac9f99c9f4b1fe38826cf
Adam-Davenport <adavenpo@purdue.edu>
2017-04-20 21:22:41 -0400

    Changing location character length

--- record/models.py
+++ record/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Mowing(models.Model):
     location = models.CharField(max_length=50)
-    direction = models.CharField(max_length=2)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    direction = models.CharField(max_length=25)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{}: {} on {}'.format(self.location, self.direction, self.date)
