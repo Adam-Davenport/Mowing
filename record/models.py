966c55f8357df4eb05db03c18f21f1cbf18c1471
adam <adavenpo@purdue.edu>
2017-04-27 19:17:53 -0400

    Allowing bank direction entry in forms

--- record/models.py
+++ record/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Mowing(models.Model):
     location = models.CharField(max_length=50)
-    direction = models.CharField(max_length=25)
+    direction = models.CharField(max_length=25, blank=True)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
